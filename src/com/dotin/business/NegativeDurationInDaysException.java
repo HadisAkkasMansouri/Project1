c18ebbddfcddc3f616e7f556074e70894ee38a83
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-06-26 09:34:11 +0430

    hi

--- src/com/dotin/business/NegativeDurationInDaysException.java
+++ src/com/dotin/business/NegativeDurationInDaysException.java
@@ -1,4 +1,8 @@
 package com.dotin.business;
 
 public class NegativeDurationInDaysException extends Exception {
+
+    public NegativeDurationInDaysException(String message){
+        super(message);
+    }
 }
