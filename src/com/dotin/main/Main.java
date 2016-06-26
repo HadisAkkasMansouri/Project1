32e77406894213926d44a23076182a3a9a6d56fb
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-06-26 16:53:07 +0430

    hello

--- src/com/dotin/main/Main.java
+++ src/com/dotin/main/Main.java
@@ -1,6 +1,8 @@
 package com.dotin.main;
 
 import com.dotin.business.XMLFileReader;
+import com.dotin.exception.NegativeDepositBalanceException;
+import com.dotin.exception.NegativeDurationInDaysException;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -16,7 +18,13 @@ public class Main {
         System.out.println("Please enter the XML file to compute \"Deposit interests\"");
         Scanner scan = new Scanner(System.in);
         String url = scan.nextLine();
-        XMLFileReader.readXMLFile(url);
+        try {
+            XMLFileReader.readXMLFile(url);
+        } catch (NegativeDepositBalanceException e) {
+            e.printStackTrace();
+        } catch (NegativeDurationInDaysException e) {
+            e.printStackTrace();
+        }
         try {
             File file = new File("D:/PayedInterestOutputFile.txt");
             if (!file.exists()) {
