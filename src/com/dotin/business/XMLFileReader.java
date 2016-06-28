af723c49d58a0865ccaf8bc71a46c2d85041ac7d
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-06-28 10:26:21 +0430

    hello

--- src/com/dotin/business/XMLFileReader.java
+++ src/com/dotin/business/XMLFileReader.java
@@ -9,12 +9,9 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
 import java.math.BigDecimal;
 
 public class XMLFileReader {
