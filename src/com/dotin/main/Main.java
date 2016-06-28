626fa2173f1fc48100c5ed5fa3ffbeecbe84f530
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-06-28 17:47:58 +0430

    hello

--- src/com/dotin/main/Main.java
+++ src/com/dotin/main/Main.java
@@ -1,12 +1,16 @@
 package com.dotin.main;
 
+import com.dotin.bean.Deposit;
 import com.dotin.business.XMLFileReader;
 import com.dotin.exception.NegativeDepositBalanceException;
 import com.dotin.exception.NegativeDurationInDaysException;
-
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,25 +18,29 @@ public class Main {
     public static void main(String argv[]) {
 
 //      "D:/DepositsFile.xml"
+        Deposit deposit = new Deposit();
         System.out.println("Please enter the XML file to compute \"Deposit interests\"");
         Scanner scan = new Scanner(System.in);
         String url = scan.nextLine();
+        List<Deposit> depositList = new ArrayList<>();
+        Collections.sort(depositList, Collections.<Deposit>reverseOrder());
         try {
             XMLFileReader.readXMLFile(url);
-        } catch (NegativeDepositBalanceException e) {
-            e.printStackTrace();
-        } catch (NegativeDurationInDaysException e) {
-            e.printStackTrace();
-        }
-        try {
             File file = new File("D:/PayedInterestOutputFile.txt");
             if (!file.exists()) {
                 file.createNewFile();
             }
-            FileWriter fileWriter = new FileWriter(file);
+            for (Deposit deposits : depositList){
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(deposits.getCustomNumber() + "#" + deposit.calculatePayedInterest(deposit.calculatePayedInterest(depositType1, depositBalance, durationInDays), "\n");
+            }
             fileWriter.close();
-            }catch(IOException e){
-                    e.printStackTrace();
+        } catch (NegativeDepositBalanceException e) {
+            e.printStackTrace();
+        } catch (NegativeDurationInDaysException e) {
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
         }
     }
 }
