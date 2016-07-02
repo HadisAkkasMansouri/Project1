d704964cd3a9eef8ad8fc7685838f654b6df11a6
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-07-02 09:37:25 +0430

    hello

--- src/com/dotin/main/Main.java
+++ src/com/dotin/main/Main.java
@@ -1,13 +1,10 @@
 package com.dotin.main;
 
 import com.dotin.bean.Deposit;
-import com.dotin.business.XMLFileReader;
+import com.dotin.business.FileReaderWriter;
 import com.dotin.exception.NegativeDepositBalanceException;
 import com.dotin.exception.NegativeDurationInDaysException;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,24 +19,12 @@ public class Main {
         System.out.println("Please enter the XML file to compute \"Deposit interests\"");
         Scanner scan = new Scanner(System.in);
         String url = scan.nextLine();
-        List<Deposit> depositList = new ArrayList<>();
-        Collections.sort(depositList, Collections.<Deposit>reverseOrder());
         try {
-            XMLFileReader.readXMLFile(url);
-            File file = new File("D:/PayedInterestOutputFile.txt");
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            for (Deposit deposits : depositList){
-                FileWriter fileWriter = new FileWriter(file);
-                fileWriter.write(deposits.getCustomNumber() + "#" + deposit.calculatePayedInterest(deposit.calculatePayedInterest(depositType1, depositBalance, durationInDays), "\n");
-            }
-            fileWriter.close();
+            FileReaderWriter.readXMLFile(url);
+
         } catch (NegativeDepositBalanceException e) {
             e.printStackTrace();
         } catch (NegativeDurationInDaysException e) {
-            e.printStackTrace();
-        } catch(IOException e){
             e.printStackTrace();
         }
     }
