804e3f7faf32e4723d5eac92c83698427af0921c
Reza Sadeghi <rezasadeghikhas@yahoo.com>
2016-07-03 09:37:27 +0430

    hello

--- src/com/dotin/main/Main.java
+++ src/com/dotin/main/Main.java
@@ -5,47 +5,28 @@ import com.dotin.business.XMLFileReader;
 import com.dotin.exception.NegativeDepositBalanceException;
 import com.dotin.exception.NegativeDurationInDaysException;
 import com.dotin.exception.OtherDepositTypeException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
 
-    public static void sortDeposits(List<Deposit> depositList){
-        Collections.sort(depositList,(Deposit first, Deposit second)-> second.compareTo(first));
-    }
-
     public static void main(String args[]) throws NegativeDurationInDaysException, NegativeDepositBalanceException, OtherDepositTypeException{
 
-
-//      "D:/DepositsFile.xml"
-        Deposit deposit = new Deposit();
-        System.out.println("Please enter the XML file to compute \"Deposit interests\"");
-        Scanner scan = new Scanner(System.in);
-        String url = scan.nextLine();
-        List<Deposit> depositList = new ArrayList<>();
-        Collections.sort(depositList, Collections.<Deposit>reverseOrder());
+        XMLFileReader.readXMLFile();
+        List<Deposit> depositList = XMLFileReader.readXMLFile();
+        Collections.sort(depositList);
+        RandomAccessFile file = null;
         try {
-            XMLFileReader.readXMLFile(url);
-            File file = new File("D:/PayedInterestOutputFile.txt");
-                if (!file.exists()) {
-                    file.createNewFile();
-                }
-                FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
-                for (Deposit deposit1 : depositList) {
-                    fileWriter.write(deposit1.getCustomNumber() + "#" + deposit1.getPayedInterest() + "\n");
-                }
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            file = new RandomAccessFile("OutputFile.txt","rw");
+            for (Deposit deposit : depositList) {
+                file.write(Integer.parseInt(deposit.getCustomNumber() + "#" + deposit.getPayedInterest() + "\n"));
             }
-          catch (NegativeDepositBalanceException e) {
+            file.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (NegativeDurationInDaysException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
